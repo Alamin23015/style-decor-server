e35b129083757668aef3cf38f17f564561a28799
Al Amin Hossain <alamin16105@gmail.com>
2025-12-07 20:06:16 +0600

    Vercel to Railway shift

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+//
 const app = express();
 const port = process.env.PORT || 5000;
 
