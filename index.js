00f2077b3902f5d9cc9dbe9ce6393302074fd62b
Al Amin Hossain <alamin16105@gmail.com>
2025-12-07 19:17:53 +0600

    Add route services

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@stylecluster.vqgtfle.mongodb.net/?appName=StyleCluster`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,14 +22,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     await client.connect();
+
+   
+    const serviceCollection = client.db("StyleDecor").collection("services");
+
+    
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
@@ -41,11 +50,6 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('StyleDecor server is running');
 })
-app.get('/services', (req, res) => {
-  res.send('StyleDecor server is running');
-})
-
-
 
 // Start Server
 app.listen(port, () => {
